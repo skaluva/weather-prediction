0ac6e14aacc2093006969b1eaddaade98edd2863
skaluva <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>
2022-06-05 20:37:31 +0530

    Initial commit

---
+++ src/main/java/com/skaluva/weather/prediction/weatherprediction/WeatherPredictionApplication.java
@@ -0,0 +1,13 @@
+package com.skaluva.weather.prediction.weatherprediction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherPredictionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherPredictionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skaluva/weather/prediction/weatherprediction/WeatherPredictionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skaluva.weather.prediction.weatherprediction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherPredictionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
